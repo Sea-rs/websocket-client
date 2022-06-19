808f1e086b49cb418794e21e1ca4856f84901dd1
sea_rs <sea_rs@github.com>
2022-06-19 13:41:28 +0900

    chatとリアクションの機能を実装

--- main.js
+++ main.js
@@ -1,40 +1,278 @@
-let chatBody = document.getElementById("chat__body");
-let btnSubmit = document.getElementById("btn--submit");
-let btnOpen = document.getElementById("btn--open");
-// let btnClose = document.getElementById("btn--close");
+/**
+ * TODO
+ * No.1 SPなど対応させる
+ * 
+ * No.x チャットのログを残す
+ */
 
-let socket = null;
+/**
+ * 同じJSが実行されたら場合にエラーを起こすことで、
+ * 重複実行されないようにする
+ */
+let isFirst = true;
 
-btnOpen.addEventListener("click", () => {
-    let socket = new WebSocket("ws://localhost:5000");
+document.addEventListener('DOMContentLoaded', () => {
+    class Socket {
+        constructor(PORT = 5000) {
+            this.SUBMIT_BTN = document.getElementById('btn__submit');
+            this.CHAT_INPUT = document.getElementById('chat__input');
+            this.BUTTON = document.getElementsByClassName('js-btn');
+            this.CHAT_RECEIVE = document.getElementById('chat__receive');
+            this.CHAT_ERROR = document.getElementById('chat__error');
 
-    removeEventListener("click", btnSubmit);
+            this.PORT = PORT;
 
-    btnSubmit.addEventListener("click", () => {
-        socket.send("hello");
-    });
+            this.SOCKET_URL = 'ws://192.168.11.7:' + this.PORT;
+            this.SOCKET = null;
 
-    socket.addEventListener("open", (e) => {
-        console.log("connected.");
-    });
+            this.isFailed = false;
+            this.timeoutTime = 0;
+            this.messageEvent = null;
+        }
+
+        /**
+         * 初期化
+         */
+        init() {
+            let that = this;
+
+            this.SOCKET = new WebSocket(this.SOCKET_URL);
+
+            let error = function () {
+                that.socketError();
+            }
+
+            let open = function () {
+                that.displayOpenMessage();
+            }
+
+            let close = function () {
+                that.displayCloseMessage();
+            }
+
+            let message = function (msgObj) {
+                if (that.messageEvent !== null && typeof that.messageEvent === 'function') {
+                    console.log(msgObj.data);
+                    that.messageEvent(msgObj.data);
+                }
+            }
+
+            this.SOCKET.removeEventListener('error', error);
+            this.SOCKET.addEventListener('error', error);
+
+            this.SOCKET.removeEventListener('open', open);
+            this.SOCKET.addEventListener('open', open);
+
+            this.SOCKET.removeEventListener('close', close);
+            this.SOCKET.addEventListener('close', close);
+
+            this.SOCKET.removeEventListener('message', message);
+            this.SOCKET.addEventListener('message', message);
+        }
+
+        socketError() {
+            this.isFailed = true;
+
+            console.log('%c接続に失敗しました', 'color: red;');
+        }
+
+        displayOpenMessage(msg) {
+            this.doEnable();
+
+            console.log('疎通成功！');
+        }
+
+        displayCloseMessage(msg) {
+            /**
+             * 再接続を行う。
+             * 
+             * 5回失敗したら接続行わない。
+             */
+            if (this.timeoutTime < 5) {
+                let reconnect = setInterval(() => {
+                    console.log('再接続する');
     
-    socket.addEventListener("close", (e) => {
-        console.log("closed.");
-    });
+                    clearInterval(reconnect);
     
-    socket.addEventListener("error", (e) => {
-        console.log("error.");
-    });
+                    this.SOCKET = null;
+                    this.init();
+                }, 5000);
+            }
+
+            if (this.isFailed) {
+                console.log('%cエラーにより接続が切れました', 'color: red;');
+            } else {
+                console.log('ネットワーク回線の不具合により接続が切れました');
+            }
+
+            this.timeoutTime++;
+
+            this.doDisable();
+        }
+
+        receiveMessage(msg) {
+            this.displayMessage(msg);
+        }
+
+        sendMessage(message) {
+            if (!message) {
+                return;
+            }
+
+            this.SOCKET.send(message);
+        }
+
+        getSendMessage() {
+            let message = this.CHAT_INPUT.value;
+
+            if (message === '') {
+                return false;
+            }
+
+            this.CHAT_INPUT.value = '';
+
+            return message;
+        }
+
+        displayMessage(msg) {
+            let timeHours = new Date().getHours().toString(10).padStart(2, '0');
+            let timeMinutes = new Date().getMinutes().toString(10).padStart(2, '0');
+            let timeSeconds = new Date().getSeconds().toString(10).padStart(2, '0');
+
+            let messageElem = document.createElement('p');
+            let text = document.createElement('span');
+            let timeStamp = document.createElement('span');
+
+            messageElem.classList.add('chat__message');
+            text.classList.add('text');
+            timeStamp.classList.add('chat__timestamp');
+
+            text.innerText = msg;
+            timeStamp.innerText = timeHours + ':' + timeMinutes + ':' + timeSeconds;
+
+            messageElem.appendChild(text);
+            messageElem.appendChild(timeStamp);
+
+            this.CHAT_RECEIVE.appendChild(messageElem);
+
+            /**
+             * 追加されたら、自動でスクロールされるようにする
+             */
+            this.CHAT_RECEIVE.scrollTop = this.CHAT_RECEIVE.scrollHeight;
+        }
+
+        /**
+         * 要素を無効化する
+         */
+        doDisable() {
+            for (let i = 0; i < this.BUTTON.length; i++) {
+                this.BUTTON[i].setAttribute('disabled', true);
+            }
+
+            this.CHAT_INPUT.setAttribute('disabled', true);
+
+            this.CHAT_ERROR.style.display = '';
+        }
+
+        /**
+         * 要素を有効化する
+         */
+        doEnable() {
+            for (let i = 0; i < this.BUTTON.length; i++) {
+                this.BUTTON[i].removeAttribute('disabled');
+            }
+
+            this.CHAT_INPUT.removeAttribute('disabled');
+
+            this.CHAT_ERROR.style.display = 'none';
+        }
+    }
+
+    loadChat();
+    reaction();
+
+    function loadChat() {
+        let socket = new Socket(5000);
     
-    socket.addEventListener("message", (e) => {
-        let newParagraph = document.createElement("p");
+        socket.init();
+        socket.messageEvent = function(msg) {
+            socket.receiveMessage(msg);
+        }
+
+        let submitBtn = document.getElementById('btn__submit');
+        let chatInput = document.getElementById('chat__input');
     
-        newParagraph.innerText = e.data;
+        let clickSend = function () {
+            let message = socket.getSendMessage();
+
+            socket.sendMessage(message);
+        }
     
-        chatBody.appendChild(newParagraph);
-    });
+        let enterSend = function (key) {
+            if (key.key === 'Enter' && key.ctrlKey) {
+                let message = socket.getSendMessage();
+
+                socket.sendMessage(message);
+            }
+        }
+    
+        submitBtn.removeEventListener('click', clickSend);
+        submitBtn.addEventListener('click', clickSend);
+    
+        chatInput.removeEventListener('keydown', enterSend);
+        chatInput.addEventListener('keydown', enterSend);
+    }
+
+    function reaction() {
+        let reactionBtn = document.getElementsByClassName('btn__reaction');
+        let reactionData = {
+            'type1' : {
+                type: 'type1'
+            },
+            'type2' : {
+                type: 'type2'
+            },
+            'type3' : {
+                type: 'type3'
+            },
+        };
+        let socket = new Socket(5001);
+
+        socket.init();
+        socket.messageEvent = function(msg) {
+            let type = JSON.parse(msg)['type'];
+            let reactionBody = document.querySelector('#reaction');
+            let query = '[data-type="' + type +'"]'
+            let moveY = 0;
+
+            let reactionElem = reactionBody.querySelector(query);
+            let reaction = reactionElem.cloneNode(true);
+
+            reaction.classList.add('shake');
+            reactionBody.appendChild(reaction);
+
+            let animation = function() {
+                if (moveY <= 400) {
+                    moveY++;
+
+                    reaction.style.top ='-' + moveY + 'px'
+
+                    requestAnimationFrame(animation);
+                } else {
+                    reactionBody.removeChild(reaction);
+                }
+            }
+
+            animation();
+        }
+
+        let reaction = function(e) {
+            let type = e.target.getAttribute('name');
+            socket.sendMessage(JSON.stringify(reactionData[type]));
+        }
+
+        for (let i = 0; i < reactionBtn.length; i++) {
+            reactionBtn[i].addEventListener('click', reaction);
+        }
+    }
 });
-
-// btnClose.addEventListener("click", () => {
-//     //
-// });
